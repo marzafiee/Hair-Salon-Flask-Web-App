b0b16165b8ca7e0e081accafb1769efebe79ac38
Inez Anne-Marie <eyra.agbenu@ashesi.edu.gh>
2025-03-15 17:44:11 +0000

    Removing all old files from the repo and replacing with updated version

--- backend/static/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("book-now").addEventListener("click", () => {
-    const message = document.getElementById("message");
-    message.textContent = "Thank you for booking! We’ll contact you soon.";
-    message.style.color = "green";
-});
