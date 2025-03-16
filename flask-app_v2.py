11724b4fbde8da15a70ce6f1b490d8f87396f6f4
InezAnne-Marie <eyra.agbenu@ashesi.edu.gh>
2025-03-16 16:45:47 +0000

    update to establish vercel deployment

--- flask-app_v2.py
+++ flask-app_v2.py
@@ -506,4 +506,4 @@ def sort_by_price():
 
 # Run the Flask application if this script is executed directly
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
